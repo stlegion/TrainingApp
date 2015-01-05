76d74035d2e0d210942fccdc013fc2082eb33ccb
Andrew <tolstyh.andrey@gmail.com>
2015-01-06 00:04:22 +0200

    1. Добавлены package; 2. Удален метод addMessage (ибо не нужен); 3. Изменена кодировка на UTF-8. такие дела

---
+++ Chat/src/chat/All.java
@@ -0,0 +1,28 @@
+package chat;
+
+import java.util.*;
+
+public class All {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Messages.addMessage2(new Date(),"�����-�� ��������� � �����-�� ��������� ������� ������1",
+				"������������52");
+		Messages.addMessage2(new Date(),"�����-�� ��������� � �����-�� ��������� ������� ������2",
+				"������������53");
+		Messages.addMessage2(new Date(),"�����-�� ��������� � �����-�� ��������� ������� ������3",
+				"������������54");
+		Messages.addMessage2(new Date(),"�����-�� ��������� � �����-�� ��������� ������� ������4",
+				"������������55");
+		Messages.addMessage2(new Date(),"�����-�� ��������� � �����-�� ��������� ������� ������5",
+				"������������56");
+		Messages.addMessage2(new Date(),"�����-�� ��������� � �����-�� ��������� ������� ������6",
+				"������������57");
+		Messages.addMessage2(new Date(),"�����-�� ��������� � �����-�� ��������� ������� ������7",
+				"������������58");
+		Messages.addMessage2(new Date(),"�����-�� ��������� � �����-�� ��������� ������� ������8",
+				"������������59");
+		Messages.getAllMessages();
+	}
+
+}
---
+++ Chat/src/chat/Messages.java
@@ -0,0 +1,16 @@
+package chat;
+
+import java.util.*;
+
+public class Messages {
+	static ArrayList<Message> messages=new ArrayList<Message>();
+	public static void addMessage2(Date date, String text, String userName){
+		messages.add(new Message(date, text, userName));
+	}
+	public static ArrayList<Message> getAllMessages(){
+		for(Message x: messages){
+			System.out.println(x.formattedText());
+		}
+		return messages;
+	}
+}
